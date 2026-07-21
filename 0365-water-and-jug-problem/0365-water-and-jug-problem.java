91328a484dd5b00c3862d57093cbae0bb84b0db4
Santoshi Patra <santoshipatra2006@gmail.com>
2026-07-21 14:24:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (42.77%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+
+      if(target > x + y) return false;
+      if(target == 0)  return true;
+
+      return target % gcd (x , y) == 0;  
+    }
+
+    public int gcd(int a, int b){
+        if(b==0)  return a;
+
+        return gcd(b, a % b);
+    }
+}
