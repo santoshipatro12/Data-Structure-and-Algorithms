63ca8d248203e0e3c339cad494037751d00f9f7c
Santoshi Patra <santoshipatra2006@gmail.com>
2026-08-08 09:40:20 +0530

    Time: 1 ms (97.52%), Space: 47.1 MB (89.18%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,12 +1,11 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int x=0, y=0;
+        int x =0, y=0;
         for(int i=0; i<nums.length; i++){
-            x+=nums[i];
+            x += nums[i];
         }
         for(int i=0; i<nums.length; i++){
             if(y== x-y-nums[i]) return i;
-
             y+= nums[i];
         }
         return -1;
