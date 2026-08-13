6c00599c9a5c5ae4611827fa7bab0abdc845af52
Santoshi Patra <santoshipatra2006@gmail.com>
2026-08-13 09:53:30 +0530

    Time: 21 ms (9.25%), Space: 50.9 MB (7.39%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,36 @@
+import java.util.*;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap <String, List<String>> map = new HashMap <>();
 
-        for(String str : strs){
-            char []arr = str.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
+        HashMap<String, List<String>> map = new HashMap<>();
 
-            if(!map.containsKey(key)){
-                map.put(key, new ArrayList <>());
-            } 
-            map.get(key).add(str);
+        for (String str : strs) {
+
+            int[] freq = new int[26];
+
+            // Count characters
+            for (char ch : str.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+
+            // Create key
+            StringBuilder key = new StringBuilder();
+
+            for (int i = 0; i < 26; i++) {
+                key.append('#');
+                key.append(freq[i]);
+            }
+
+            // Create group if key doesn't exist
+            if (!map.containsKey(key.toString())) {
+                map.put(key.toString(), new ArrayList<>());
+            }
+
+            // Add string to its group
+            map.get(key.toString()).add(str);
         }
-        return new ArrayList <>(map.values());
+
+        return new ArrayList<>(map.values());
     }
 }
