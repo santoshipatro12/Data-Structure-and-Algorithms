b0ac6f187ec59a6a22924f48033667dbc382e3cb
Santoshi Patra <santoshipatra2006@gmail.com>
2026-09-22 18:26:29 +0530

    Time: 16 ms (39.41%), Space: 47.8 MB (33.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,30 +2,17 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap <Integer, Integer> map = new HashMap <>();
 
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for(int num :nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
 
-        List <Integer> [] bucket = new List [nums.length + 1];
+        ArrayList <Map.Entry<Integer, Integer>> List = new ArrayList<>(map.entrySet());
 
-        for(int num : map.keySet()){
-            int freq = map.get(num);
-            if(bucket[freq] == null){
-                bucket[freq] = new ArrayList <>();
-            }
-            bucket[freq].add(num);
-        }
+        List.sort((a,b) -> b.getValue()-a.getValue());
 
         int []result = new int[k];
-        int index =0;
-        for(int freq = nums.length ; freq >=0; freq --){
-            if(bucket[freq]!= null){
-                for(int num : bucket[freq]){
-                    result[index] = num;
-                    index++;
-                    if(index == k) return result;
-                }
-            }
+        for(int i=0; i<k;i++){
+            result[i] = List.get(i).getKey();
         }
         return result;
     }
