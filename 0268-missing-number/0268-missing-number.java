cd1a13b8b0677c7935ea8cca05652aeca49737bf
Santoshi Patra <santoshipatra2006@gmail.com>
2026-09-25 23:51:44 +0530

    Time: 1 ms (28.59%), Space: 47.1 MB (91%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,28 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-
-        int temp =0, i=0;
+        int i=0;
         while(i<nums.length){
-            if(temp == nums[i]){
-                i++; temp++;
+            if(nums[i]>=nums.length){
+                i++;
+                continue;
+            }
+            int crtIdx = nums[i];
+            if(nums[i]!= nums[crtIdx]){
+                swap(nums, i, crtIdx);
             }else{
-                return temp;
+                i++;
             }
         }
-        return temp;
+        for(int j=0; j<nums.length; j++){
+            if(j!=nums[j]) return j;
+        }
+        return nums.length;
     }
+    
+
+        public void swap(int[]nums, int i, int crtIdx){
+            int temp = nums[i];
+            nums[i] = nums[crtIdx];
+            nums[crtIdx] = temp;
+        }
 }
